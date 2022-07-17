038bbc1de4862d8bc2f8cfdbe91ef8c6ee45a996
Alexander Furmanov <kymapcah61@gmail.com>
2022-07-17 23:00:26 +0300

    Finish home tasks

---
+++ Task_1/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+ Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+
+*/
+
+int GetNumber(string messange)
+{
+    int result = 0;
+    Console.WriteLine(messange);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы не ввели число. Введите число");
+        }
+
+    }
+    return result;
+}
+
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+int[] GetArray(int number)
+{
+    int result = 0;
+    int[] array = new int[5];
+
+
+
+    for (int i = 0; i < array.Length; i++)
+    {
+
+
+        array[i] = GetNumber("Enter number");
+
+
+    }
+
+    return array;
+}
+
+
+int FindPositivNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+
+}
+
+int[] array = GetArray(1);
+PrintArray(array);
+Console.WriteLine();
+int result = FindPositivNumbers(array);
+Console.WriteLine($"Колличество чисел больше 0  -  {result}");
---
+++ Task_2/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+
+Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+ значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)_ * опечатка в варинате
+
+*/
+//  x=(b2-b1)/(k1-k2) , y=k1(b2-b1)/(k1-k2)+b1
+//  k2 * x + b2 =  k1 * x + b1
+
+
+int GetNumber(string messange)
+{
+    int result = 0;
+    Console.WriteLine(messange);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы не ввели число. Введите число");
+        }
+
+    }
+    return result;
+}
+
+
+// int b1 = GetNumber("Enter b1: ");
+// int k1 = GetNumber("Enter k1: ");
+// int b2 = GetNumber("Enter b2: ");
+// int k2 = GetNumber("Enter k2: ");
+
+double b1 = 2;
+double k1 = 5;
+double b2 = 4;
+double k2 = 9;
+
+
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*(b2-b1)/(k1-k2)+b1;
+
+
+if ( x == y)
+Console.WriteLine($"прямые пересекаются в точке ({x}, {y})");
+else
+Console.WriteLine($"прямые не пересекаются");
+
+
+
